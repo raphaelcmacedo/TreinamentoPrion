81d1d5725fb7edc171d3cab4f596a92b4da4c1c4
raphaelcmacedo <raphaelcmacedo@hotmail.com>
2017-07-20 08:46:06 -0300

    Commit para exercicio

---
+++ Business/Service/GenericService.cs
@@ -0,0 +1,53 @@
+﻿using Business.DAO;
+using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Service
+{
+    public class GenericService<TEntity> where TEntity : GenericModel
+    {
+
+        protected readonly GenericDAO<TEntity> dao;
+
+        public GenericService(GenericDAO<TEntity> dao)
+        {
+            this.dao = dao;
+        }
+
+        public virtual void Add(TEntity entity)
+        {
+            Validate(entity);
+            dao.Add(entity);
+        }
+        public void Update(TEntity entity)
+        {
+            Validate(entity);
+            dao.Update(entity);
+        }
+        public virtual void Validate(TEntity entity)
+        {
+
+        }
+        public TEntity Find(int id)
+        {
+            return dao.Find(id);
+        }
+        public IEnumerable<TEntity> ListAll()
+        {
+            return dao.ListAll();
+        }
+        public void Delete(TEntity entity)
+        {
+            dao.Delete(entity);
+        }
+
+        public void Dispose()
+        {
+            dao.Dispose();
+        }
+    }
+}
---
+++ Business/Service/PessoaService.cs
@@ -0,0 +1,24 @@
+﻿using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.DAO;
+
+namespace Business.Service
+{
+    public class PessoaService:GenericService<Pessoa>
+    {
+        public PessoaService():base(new PessoaDAO())
+        {
+
+        }
+
+        public Pessoa Find(string name)
+        {
+            //O PROBLEMA ESTÁ AQUI
+            base.dao.Fin
+        }
+    }
+}
